b153723a4ceb650d60ee5182ef8469c49746e925
Enrique Pando <enr.pando@gmail.com>
2025-04-04 15:21:37 -0600

    projects page base

---
+++ src/lib/components/ProjectFrame.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	let {
+		width,
+		height,
+	}: {
+		width: number;
+		height: number;
+	} = $props();
+
+	const frameSize = 0.25;
+	const frameInnerDepth = 0.25;
+	const frameOuterDepth = 0.75;
+</script>
+
+<div
+	style={`width: ${width}em; height: ${height}em; translate: ${-width / 2}em ${-height / 2}em 0;`}
+	class="absolute backface-hidden"
+>
+	<div class="size-full bg-red-400"></div>
+</div>
+
+<div
+	style={`width: ${width}em; height: ${frameInnerDepth}em; translate: ${-width / 2}em ${-height / 2}em; rotate: x 270deg;`}
+	class="absolute backface-hidden origin-bottom-left bottom-0 bg-zinc-500"
+></div>
+<div
+	style={`width: ${width}em; height: ${frameInnerDepth}em; translate: ${-width / 2}em ${height / 2}em; rotate: x 90deg;`}
+	class="absolute backface-hidden origin-top-left bg-zinc-100"
+></div>
+<div
+	style={`width: ${frameInnerDepth}em; height: ${height}em; translate: ${-width / 2}em ${-height / 2}em; rotate: y 90deg;`}
+	class="absolute backface-hidden origin-top-right right-0 bg-zinc-400"
+></div>
+<div
+	style={`width: ${frameInnerDepth}em; height: ${height}em; translate: ${width / 2}em ${-height / 2}em; rotate: y 270deg;`}
+	class="absolute backface-hidden origin-top-left bg-zinc-300"
+></div>
+
+<svg
+	style={`width: ${width + (frameSize * 2)}em; height: ${height + (frameSize * 2)}em; translate: ${(-width / 2) - frameSize}em ${(-height / 2) - frameSize}em ${frameInnerDepth}em;`}
+	class="absolute backface-hidden fill-zinc-200"
+	viewBox={`0 0 ${width + (frameSize * 2)} ${height + (frameSize * 2)}`}
+>
+	<path d={`M 0 0 L 0 ${height + (frameSize * 2)} L ${width + (frameSize * 2)} ${height + (frameSize * 2)} L ${width + (frameSize * 2)} 0 L 0 0 z M ${frameSize} ${frameSize} L ${width + frameSize} ${frameSize} L ${width + frameSize} ${height + frameSize} L ${frameSize} ${height + frameSize} L ${frameSize} ${frameSize} z`} />
+</svg>
+
+<div
+	style={`width: ${width + (frameSize * 2)}em; height: ${frameInnerDepth + frameOuterDepth}em; translate: ${(-width / 2) - frameSize}em ${(-height / 2) - frameSize}em ${frameInnerDepth}em; rotate: x 90deg;`}
+	class="absolute backface-hidden origin-bottom-left bottom-0 bg-zinc-100"
+></div>
+<div
+	style={`width: ${width + (frameSize * 2)}em; height: ${frameInnerDepth + frameOuterDepth}em; translate: ${(-width / 2) - frameSize}em ${(height / 2) + frameSize}em ${frameInnerDepth}em; rotate: x 270deg;`}
+	class="absolute backface-hidden origin-top-left bg-zinc-500"
+></div>
+<div
+	style={`width: ${frameInnerDepth + frameOuterDepth}em; height: ${height + (frameSize * 2)}em; translate: ${(-width / 2) - frameSize}em ${(-height / 2) - frameSize}em ${frameInnerDepth}em; rotate: y 270deg;`}
+	class="absolute backface-hidden origin-top-right right-0 bg-zinc-300"
+></div>
+<div
+	style={`width: ${frameInnerDepth + frameOuterDepth}em; height: ${height + (frameSize * 2)}em; translate: ${(width / 2) + frameSize}em ${(-height / 2) - frameSize}em ${frameInnerDepth}em; rotate: y 90deg;`}
+	class="absolute backface-hidden origin-top-left bg-zinc-400"
+></div>
---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,172 @@
+<script lang="ts">
+	import ProjectFrame from "$lib/components/ProjectFrame.svelte";
+	import { type rootLayoutCtx, rootLayoutKey } from "$lib/context";
+	import { scroll } from "motion";
+	import { getContext, onMount } from "svelte";
+	import { Spring } from "svelte/motion";
+
+	const {
+		sectionTransitionActive,
+	} = getContext<rootLayoutCtx>(rootLayoutKey);
+
+	// Section navigation timing
+
+	onMount(() => {
+		const transitionTimeout = setTimeout(() => {
+			$sectionTransitionActive = false;
+		}, 800);
+
+		return () => {
+			clearTimeout(transitionTimeout);
+		};
+	});
+
+	// Waving animation
+
+	let wave1 = new Spring(0, { damping: 0.5, stiffness: 0.05 });
+	let wave2 = new Spring(0, { damping: 0.5, stiffness: 0.05 });
+	let wave3 = new Spring(0, { damping: 0.5, stiffness: 0.05 });
+
+	onMount(() => {
+		let interval = setInterval(() => {
+			wave1.target = Math.cos(performance.now() / 500 + 1.5) * 1.2;
+			wave2.target = Math.cos(performance.now() / 500 + 0.75) * 0.5;
+			wave3.target = Math.cos(performance.now() / 500) * 1.7;
+		}, 1000 / 30);
+
+		return () => clearInterval(interval);
+	});
+
+	// Mouse
+
+	let mx = $state(0);
+	let my = $state(0);
+
+	let sw = $state(0);
+	let sh = $state(0);
+
+	onMount(() => {
+		const mouseListener = (e: MouseEvent) => {
+			mx = e.clientX;
+			my = e.clientY;
+		};
+
+		const resizeListener = () => {
+			sw = window.innerWidth;
+			sh = window.innerHeight;
+
+			mx = sw / 2;
+			my = sh / 2;
+		};
+
+		resizeListener();
+
+		mx = sw / 2;
+		my = sh / 2;
+
+		addEventListener("mousemove", mouseListener);
+		addEventListener("resize", resizeListener);
+
+		return () => {
+			removeEventListener("mousemove", mouseListener);
+			removeEventListener("resize", resizeListener);
+		};
+	});
+
+	// Infinite scroll
+
+	let scrollRegionIndex = $state(0);
+	let scrollContainer: HTMLElement;
+
+	const scrollRegionAmount = 7; // 3 is the minimum but a bigger number allows faster scroll
+	const scrollRegionAmountOneSide = Math.trunc(scrollRegionAmount / 2);
+
+	onMount(() => {
+		function getMiddleRegionRect() {
+			return scrollContainer.children[scrollRegionAmountOneSide].getBoundingClientRect();
+		}
+
+		scrollContainer.scrollBy(0, getMiddleRegionRect().top);
+
+		scroll((progress: number) => {
+			if (progress > (scrollRegionAmountOneSide + 0.5) / (scrollRegionAmount - 1)) {
+				scrollRegionIndex++;
+			}
+
+			if (progress < (scrollRegionAmountOneSide - 0.5) / (scrollRegionAmount - 1)) {
+				scrollRegionIndex--;
+			}
+		}, {
+			container: scrollContainer,
+		});
+
+		const snapInstant = () => {
+			scrollContainer.scrollBy(0, getMiddleRegionRect().top);
+		};
+		const snapSmooth = () => {
+			scrollContainer.scrollBy({
+				top: getMiddleRegionRect().top,
+				behavior: "smooth",
+			});
+		};
+
+		let timeout: number;
+
+		const snapSmoothDebounce = () => {
+			clearTimeout(timeout);
+			timeout = setTimeout(snapSmooth, 300);
+		};
+
+		scrollContainer.addEventListener("scroll", snapSmoothDebounce);
+		window.addEventListener("resize", snapInstant);
+
+		return () => {
+			scrollContainer.removeEventListener("scroll", snapSmoothDebounce);
+			window.removeEventListener("resize", snapInstant);
+		};
+	});
+</script>
+
+<div class="absolute inset-0">
+	<div class="flex justify-center py-12">
+		<span class="font-semibold font-stretch-110% text-3xl">
+			PROJECTS
+		</span>
+	</div>
+	<div class="absolute inset-0 overflow-auto">
+		<div class="absolute left-[50vw] top-[50vh] text-[min(3vw,4vh)] perspective-[26em]">
+			<div
+				style={`transform: rotateX(${(my - (sh / 2)) / -(sh / 360)}deg) rotateY(${(mx - (sw / 2)) / (sw / 360)}deg);`}
+				class="transform-3d"
+			>
+				<div
+					style={`translate: -11.5em ${wave1.current - 1}em 0;`}
+					class="transform-3d"
+				>
+					<ProjectFrame height={7} width={3} />
+				</div>
+				<div
+					style={`translate: 0 ${wave2.current}em 0;`}
+					class="transform-3d"
+				>
+
+					<ProjectFrame height={12} width={16} />
+				</div>
+				<div
+					style={`translate: 10.75em ${wave3.current + 1}em 0;`}
+					class="transform-3d"
+				>
+					<ProjectFrame height={4} width={1.5} />
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="absolute inset-0 z-50">
+		<div class="absolute">{scrollRegionIndex}</div>
+		<div bind:this={scrollContainer} class="h-screen overflow-auto opacity-70">
+			{#each [...Array(scrollRegionAmount)].map((_, i) => scrollRegionIndex + i) as i (i)}
+				<div style={`background: hsl(${i * 20}, 50%, 30%);`} class="h-screen grid place-items-center">{i} <br /> {Math.random() * 1000 | 0}</div>
+			{/each}
+		</div>
+	</div>
+</div>
